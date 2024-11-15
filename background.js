aa0ea0b34ea87e0c7e399daa86cd98607c4a3525
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2024-11-15 18:16:44 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
 chrome.commands.onCommand.addListener(async (command) => {
   if (command === "cycle-next-tab") {
-    // Get all open tabs in the current window
+
     let [currentWindow] = await chrome.windows.getAll({populate: true, windowTypes: ["normal"]});
     let tabs = currentWindow.tabs;
     let activeTab = tabs.find(tab => tab.active);
     
-    // Determine the next tab's index, wrapping around if at the last tab
     let nextTabIndex = (activeTab.index + 1) % tabs.length;
     
-    // Switch to the next tab
     chrome.tabs.update(tabs[nextTabIndex].id, {active: true});
   }
 });
