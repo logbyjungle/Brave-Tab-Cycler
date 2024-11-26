da196b51732badbd122052da02efb0e8a723ce67
Jungle_log <90323544+logbyjungle@users.noreply.github.com>
2024-11-26 09:56:47 +0000

    it now works on the correct window

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 chrome.commands.onCommand.addListener(async (command) => {
   if (command === "cycle-next-tab") {
-
-    let [currentWindow] = await chrome.windows.getAll({populate: true, windowTypes: ["normal"]});
+    // Get all open tabs in the current window
+    // let [currentWindow] = await chrome.windows.getAll({populate: true, windowTypes: ["normal"]});
+	let currentWindow = await chrome.windows.getCurrent({populate: true});
     let tabs = currentWindow.tabs;
     let activeTab = tabs.find(tab => tab.active);
     
+    // Determine the next tab's index, wrapping around if at the last tab
     let nextTabIndex = (activeTab.index + 1) % tabs.length;
     
+    // Switch to the next tab
     chrome.tabs.update(tabs[nextTabIndex].id, {active: true});
   }
 });
